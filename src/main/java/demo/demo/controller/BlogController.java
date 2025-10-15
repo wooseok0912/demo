bb989bfe3be4939614ffc6ffe5f5c6601338d1ba
woo <dntjr0912@naver.com>
2025-10-15 13:29:26 +0900

    6주차

--- src/main/java/demo/demo/controller/BlogController.java
+++ src/main/java/demo/demo/controller/BlogController.java
@@ -2,14 +2,20 @@ package demo.demo.controller;
 import demo.demo.model.domain.Article;
 import demo.demo.model.domain.testdb;
 import demo.demo.model.service.TestService; // 최상단 서비스 클래스 연동 추가
+import demo.demo.model.service.AddArticleRequest;
 import demo.demo.model.service.BlogService;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller // 컨트롤러 어노테이션 명시
 public class BlogController {
@@ -71,4 +77,29 @@ public class BlogController {
         model.addAttribute("articles", list); // 모델에 추가
         return "article_list"; // .HTML 연결
     }
+
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+
+        if (list.isPresent()) {
+            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+        } else {
+            // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+            return "/error_page/article_error"; // 오류 처리 페이지로 연결(이름 수정됨)
+        }
+        return "article_edit"; // .HTML 연결
+    }
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
+    @DeleteMapping("/api/article_delete/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+        return "redirect:/article_list";
+    }
+
 }
--- src/main/java/demo/demo/model/domain/Article.java
+++ src/main/java/demo/demo/model/domain/Article.java
@@ -18,8 +18,14 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content = "";
     @Builder // 생성자에 빌더 패턴 적용(불변성)
-    public Article(String title, String content){
+
+    public Article(String title, String content) {
         this.title = title;
         this.content = content;
-        }
+    }
+
+    public void update(String title, String content) { // 현재 객체 상태 업데이트
+        this.title = title;
+        this.content = content;
+    }
 }
